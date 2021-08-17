b498bd6062ee9be304956554b0754fb32bcb488c
Krystian Plich <KrystianPlich@github.com>
2021-08-17 13:18:21 +0200

    External func - assertions updated

--- spec/actions.ts
+++ spec/actions.ts
@@ -3,7 +3,7 @@ import { browser, $, $$, element, by, protractor } from "protractor";
 export async function select(idx, item) {
 
     let until = protractor.ExpectedConditions;
-    let itemToSelect = element(by.xpath(`//li[text()="${item}"]`))
+    let itemToSelect = await element(by.xpath(`//li[text()="${item}"]`))//
 
     await $$('span.k-select').get(idx).click();
     await browser.wait(until.elementToBeClickable(itemToSelect), 5000);				 
@@ -13,6 +13,11 @@ export async function select(idx, item) {
 	
 }
 
-// export async function getSelectedItem(...) {
-//     return await ...
-// }
+export async function getSelectedItem(item, attribute) {
+
+    let until = protractor.ExpectedConditions;
+    let itemToSelect = item;
+
+    await browser.wait(until.presenceOf(itemToSelect), 5000);
+    return itemToSelect.getAttribute(attribute);
+}
--- spec/external-functions.spec.ts
+++ spec/external-functions.spec.ts
@@ -1,5 +1,5 @@
 import { browser, $, $$, element, by, protractor } from "protractor";
-import { select } from "./actions";
+import { select , getSelectedItem} from "./actions";
 
 describe("Wyszukiwarka", function () {
 
@@ -16,14 +16,14 @@ describe("Wyszukiwarka", function () {
     expect(currentWidth).toEqual(newWidth);
   });
 
-  //
-  // Zastąp bezpośrednie odwołanie do funckji getAttribute odwołaniem do funkcji w zewnętrznym pliku
-  //
-  // it("umożliwiać zmianę domyślnej szerokości opon", async function () {
-  //   let newWidth = "225";
-  //   await select(0, newWidth)
-  //   const currentWidth = getSelectedItem(...)
-  //   expect(currentWidth).toEqual(newWidth);
-  // });
+  
+  //Zastąp bezpośrednie odwołanie do funckji getAttribute odwołaniem do funkcji w zewnętrznym pliku
+  
+  it("umożliwiać zmianę domyślnej szerokości opon", async function () {
+    let newWidth = "225";
+    await select(0, newWidth)
+    const currentWidth = getSelectedItem($('#opona-szerokosc'), 'value');
+    expect(currentWidth).toEqual(newWidth);
+  });
 
 });
